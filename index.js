4f24456fc7136e554dd11e2ce43f90c90e69ec33
Wesley Chen <weschen1996@gmail.com>
2020-03-09 16:42:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+<<<<<<< HEAD
 const num1 = 2
 const num2 = 31
 const multiply = num1 * num2
 const random = Math.floor( Math.random() * 4 )
 const mod = 9 % 5
 const max = Math.max( 5, 10, 15, 20 )
+=======
+
+// Write your code here
+
+let multiply (num1, num2){
+  return 62
+}
+
+let random = Math.floor(Math.random() * 4)
+let mod = 9%5
+let max = 20
+>>>>>>> ed063b140633d7501088cd1c1b22943806928c42
